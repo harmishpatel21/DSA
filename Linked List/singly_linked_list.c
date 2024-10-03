4ba161434dc6f0c223d9f6cc6ff2b3331e9f9ae3
Harmish Patel <hpatel@entegrasolutions.com>
2024-10-03 15:47:51 -0400

    Added Linked List in py

--- Linked List/singly_linked_list.c
+++ Linked List/singly_linked_list.c
@@ -41,6 +41,41 @@ void insertAtEnd(int data) {
     current->next = newNode;
 }
 
+void insertAfterNode(struct node *list, int data) {
+    struct node *newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->next = list->next;
+    list->next = newNode;
+}
+
+void deleteAtBegin() {
+    if(head == NULL) {
+        return;
+    }
+    struct node* temp = head;
+    head = head->next;
+    free(temp);
+}
+
+void deleteAtEnd() {
+    if(head == NULL) {
+        return;
+    }
+
+    if(head->next == NULL) {
+        free(head);
+        head = NULL;
+        return;
+    }
+    struct node* temp = head;
+    while(temp->next->next != NULL) {
+        temp = temp->next;
+    }
+    free(temp->next);
+    temp->next = NULL;
+
+}
+
 int main() {
     
     insertAtEnd(100);
@@ -49,8 +84,16 @@ int main() {
     insertAtBegin(1);
     printList();
     printf("\n");
+    deleteAtBegin();
+    printList();
+    printf("\n");
     insertAtBegin(2);
+    insertAfterNode(head->next, 50);
     insertAtEnd(3);
+    deleteAtEnd();
+    printList();
+    printf("\n");
+   
     insertAtEnd(1);
     insertAtEnd(10);
     printList();
---
+++ Linked List/singly_linked_list.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None 
+    
+    def print_list(self):
+        current = self.head
+        while current:
+            print(f"{current.data} -> ", end="")
+            current = current.next
+        print("None")
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+    
+    def insert_at_begin(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete_at_begin(self):
+        if self.head is None:
+            return
+        self.head = self.head.next
+
+    def delete_at_end(self):
+        if self.head is None:
+            return
+        
+        if self.head.next is None:
+            self.head = None
+            return
+        
+        current = self.head
+        while current.next.next is not None:
+            current = current.next
+        current.next = None
+
+
+if __name__ == "__main__":
+    sll = SinglyLinkedList()
+    
+    sll.print_list()
+    sll.insert_at_end(1)
+    sll.insert_at_end(2)
+    sll.insert_at_end(3)
+    sll.print_list()
+    sll.insert_at_begin(10)
+    sll.insert_at_begin(12)
+    sll.print_list()
+    sll.insert_at_begin(19)
+    sll.print_list()
+    sll.delete_at_begin()
+    sll.print_list()
+    sll.insert_at_end(100)
+    sll.print_list()
+    sll.delete_at_end()
+    sll.print_list()
